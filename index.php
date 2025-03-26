63a70c17a6f8a5fb9cc320c92c35a91c7de1d596
El gonza del cap <ggmartinez.utu@gmail.com>
2025-03-26 20:47:43 -0300

    Recupero los patos

---
+++ index.php
@@ -0,0 +1,31 @@
+<h1> Lista de Patos </h1>
+<?php
+$servername = getenv("DB_SERVER");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_NAME");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query
+$sql = "SELECT * FROM patos";
+$result = $conn->query($sql);
+
+// Display data
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Name: " . $row["nombre"] . "<br>";
+    }
+} else {
+    echo "No records found.";
+}
+
+// Close connection
+$conn->close();
+?>
